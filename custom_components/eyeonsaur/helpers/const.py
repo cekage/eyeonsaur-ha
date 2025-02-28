fba431b8b9daeb90a86af54cf77850c186a7bc60
Cekage Dave <github@cekage.net>
2025-02-28 07:12:03 +0000

    fix(const): Fix DEV mode and add CONF_CLIENT_ID
    - Set DEV to True at runtime
    - Added CONF_CLIENT_ID constant

--- custom_components/eyeonsaur/helpers/const.py
+++ custom_components/eyeonsaur/helpers/const.py
@@ -9,6 +9,7 @@ from homeassistant.const import (
     ATTR_MANUFACTURER,
     ATTR_MODEL,
     ATTR_SERIAL_NUMBER,
+    CONF_CLIENT_ID,
     CONF_DISCOVERY,
     CONF_EMAIL,
     CONF_PASSWORD,
@@ -22,7 +23,7 @@ PLATFORMS: Final = ["sensor"]
 if TYPE_CHECKING:
     DEV: Final[bool] = False  # During type checking, DEV is False
 else:
-    DEV: Final[bool] = False  # At runtime !
+    DEV: Final[bool] = True  # At runtime !
 
 DEFAULT_POLLING_INTERVAL = timedelta(hours=12)
 DEV_POLLING_INTERVAL = timedelta(minutes=5)
@@ -33,12 +34,13 @@ ENTRY_LOGIN: Final = CONF_EMAIL
 ENTRY_PASS: Final = CONF_PASSWORD
 ENTRY_UNDERSTAND: Final = CONF_DISCOVERY
 ENTRY_TOKEN: Final = CONF_TOKEN
-ENTRY_UNIQUE_ID: Final = CONF_UNIQUE_ID
+ENTRY_COMPTEURID: Final = CONF_UNIQUE_ID
 ENTRY_MANUFACTURER: Final = ATTR_MANUFACTURER
 ENTRY_MODEL: Final = ATTR_MODEL
 ENTRY_SERIAL_NUMBER: Final = ATTR_SERIAL_NUMBER
 ENTRY_CREATED_AT: Final = "created_at"
 ENTRY_ABSOLUTE_CONSUMPTION: Final = "absolute_consumption"
+ENTRY_CLIENTID: Final = CONF_CLIENT_ID
 
 USERNAME: Final = "john@example.com"  # Adresse email du client
 PASSWORD: Final = "FAKEPASSWORD"  # Mot de passe du client
